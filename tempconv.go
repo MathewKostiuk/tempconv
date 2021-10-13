53fc41ab330ad2d04d48959734c810ba7c0194d3
MathewKostiuk <mathewkostiuk@gmail.com>
2021-10-12 20:22:27 -0700

    Added celsius flag

--- tempconv.go
+++ tempconv.go
@@ -7,6 +7,8 @@ type Celsius float64
 type Fahrenheit float64
 type Kelvin float64
 
+type celsiusFlag struct{ Celsius }
+
 const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
@@ -16,3 +18,18 @@ const (
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	}
+	return fmt.Errorf("invalid temperate %q", s)
+}
